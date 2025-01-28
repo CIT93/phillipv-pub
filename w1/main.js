36daeddf6e4baf8f490e15de4f63818b44722231
Phillip Vongsouthi <phillipvongsouthi@gmail.com>
2025-01-27 22:41:51 -0800

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,22 @@
-console.log("Week 1 - Hello World");
+console.log("hello from inside the main.js file");
+
+//let myVar;
+
+const myVar = "Phillip";
+const myVarType = typeof(myVar);
+console.log("myVarType " + myVarType);
+console.log('myVarType" ${myVarType}');
+
+
+
+function runNow () {
+    if(myVarType === "string") {
+        console.log('this 11 line will run');
+    }   else {
+        console.log('this 16 line will not run');
+    }
+}
+
+runNow();
+runNow();
+runNow();
